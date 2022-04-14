a089ed6c898ae8b5200d37474ac06b4993ca52f2
sahinboga <bogasahin967@gmail.com>
2022-04-14 20:51:59 +0300

    Update constants.js

--- Frontend/hrms/src/utils/constants.js
+++ Frontend/hrms/src/utils/constants.js
@@ -1,13 +1,15 @@
 
 export default class Constant {
 
-    static employerId = 10
-    static JobSeekerId = 2
-    static UserRole =3
-    static Roles = {
-        Admin: 1,
-        Employer: 2,
-        JobSeeker: 3,
+
+    static employerId=2
+    static JobSeekerId=2
+    static UserRole=3
+    static Roles={
+        Admin:1,
+        JobSeeker:2,
+        Employer:3
+
     }
 }
 
